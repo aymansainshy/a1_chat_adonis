c4e691e01c03b51e6c1aa6144574dab0778eaaa4
aymansainshy <aymansainshy@gmail.com>
2023-01-07 21:43:38 +0200

    First commit

---
+++ database/migrations/1673110791323_users.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class User extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name').defaultTo('')
+      table.string('phone_number')
+      table.string('image_url').defaultTo('')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
