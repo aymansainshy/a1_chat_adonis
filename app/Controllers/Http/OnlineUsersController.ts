ef2b4ce9bfd74e111f4bef624933c558bc925880
aymansainshy <aymansainshy@gmail.com>
2023-01-23 17:59:59 +0200

    Add online user route

---
+++ app/Controllers/Http/OnlineUsersController.ts
@@ -0,0 +1,26 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import socketContainer from "App/helper/OnlineUserContainer";
+
+export default class OnlineUsersController {
+    public async getOnlineUsers(ctx: HttpContextContract) {
+        try {
+
+            var onlineUser = Array.from(socketContainer().values()) 
+            console.log(onlineUser);
+    
+            return ctx.response.status(200).send({
+                code: 1,
+                message: 'founded data',
+                data: onlineUser,
+            });
+        } catch (e) {
+            console.log(e);
+            return ctx.response.status(410).send({
+                code: 0,
+                message: 'Error ',
+                data: e
+            });
+        }
+    }
+}
--- start/socket.ts
+++ start/socket.ts
@@ -1,15 +1,41 @@
+import socketContainer from 'App/helper/OnlineUserContainer'
 import Ws from '../app/Services/Ws'
 Ws.boot()
+
+
+let container = socketContainer();
 
 /**
  * Listen for incoming socket connections
  */
 
 Ws.io?.on('connection', (socket) => {
-  socket.emit('news', { hello: 'world' })
+
+  
+  socket.on('user-data' , (data) => {
+    container.set( data.user.id , {...data.user, socketId: socket.id })
+
+    console.log(socket.id)
+    // console.log(container);
+
+    socket.broadcast.emit('online-user', container.get(data.user.id))
+  })
 
   socket.on('send-message', (data) => {
     console.log(data.content)
     console.log(data.sender.phoneNumber)
+  })
+
+
+  socket.on('disconnected-user-data', (user) => {
+    console.log('DisConnected.....')
+    console.log(user)
+    
+    container.delete(user.id);
+  })
+
+
+  socket.on('disconnect' ,() => {
+    console.log('DisConnected')
   })
 })
